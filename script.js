ebe2d2c8609c62a36e700f3a0733a1c529685271
Encinia <sebastienencinia@my.whccd.edu>
2020-02-24 21:07:36 -0800

    Added prevent default to my youtube link

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 let button = document.querySelector("button");
+let link = document.getElementById("myFavWebsite");
 
-function buttonClick(){
-    alert("This is an alert!");
-    button.removeEventListener("click", buttonClick);
+function buttonClick() {
+  alert("This is an alert!");
+  button.removeEventListener("click", buttonClick);
 }
 
-button.addEventListener("click", buttonClick)
+button.addEventListener("click", buttonClick);
+link.addEventListener("click", event =>{
+    event.preventDefault();
+    alert("No youtube for lazy people!");
+});
